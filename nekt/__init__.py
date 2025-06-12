98a913fda4081b486cdec95f3bb6ee7bc13ccfc4
Andrio Frizon <andrio@nekt.ai>
2025-06-12 13:27:28 -0300

    feat: Add create_spark_session method

--- nekt/__init__.py
+++
@@ -1,6 +0,0 @@
-class TransformationClient:
-    def __init__(self, data_access_token: str):
-        self.data_access_token = data_access_token
-
-    def transform(self, data: dict) -> dict:
-        return data
